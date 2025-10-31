5ffcdc9e6180901726385a93c69c88a3003bd96a
gobdanov <gobdanov@inbox.ru>
2025-10-31 09:40:13 +0500

    Созданные интерфейсы. Созданные дополнительные классы.

---
+++ YP_API/Helpers/PaginationParams.cs
@@ -0,0 +1,38 @@
+﻿namespace YP_API.Helpers
+{
+    public class PaginationParams
+    {
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+    }
+
+    public class PagedList<T> : List<T>
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+
+        public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+        {
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            PageSize = pageSize;
+            TotalCount = count;
+            AddRange(items);
+        }
+
+        public static PagedList<T> Create(IEnumerable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
---
+++ YP_API/Helpers/RecipeSearchParams.cs
@@ -0,0 +1,26 @@
+﻿namespace YP_API.Helpers
+{
+    public class RecipeSearchParams : PaginationParams
+    {
+        public string Name { get; set; }
+        public List<string> Tags { get; set; } = new List<string>();
+        public List<string> ExcludedAllergies { get; set; } = new List<string>();
+        public List<int> IncludeIngredients { get; set; } = new List<int>();
+        public List<int> ExcludeIngredients { get; set; } = new List<int>();
+        public int? MaxPrepTime { get; set; }
+        public int? MaxCookTime { get; set; }
+        public decimal? MaxCalories { get; set; }
+        public string SortBy { get; set; } = "name";
+        public bool SortDescending { get; set; } = false;
+    }
+
+    public class IngredientSearchParams : PaginationParams
+    {
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public int? CategoryId { get; set; }
+        public bool? HasAllergies { get; set; }
+        public string SortBy { get; set; } = "name";
+        public bool SortDescending { get; set; } = false;
+    }
+}
