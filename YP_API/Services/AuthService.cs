9356775015f6e84e637b313c85ccc79fcd8674a0
gobdanov <gobdanov@inbox.ru>
2025-10-31 11:20:50 +0500

    Созданные сервисы. Кантуганов

---
+++ YP_API/Services/AuthService.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using YP_API.DTOs;
+using YP_API.Interfaces;
+using YP_API.Models;
+
+namespace YP_API.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IConfiguration _config;
+
+        public AuthService(IUserRepository userRepository, IConfiguration config)
+        {
+            _userRepository = userRepository;
+            _config = config;
+        }
+
+        public async Task<UserDto> Register(RegisterDto registerDto)
+        {
+            if (await _userRepository.UserExistsAsync(registerDto.Username, registerDto.Email))
+                throw new Exception("Username or email already exists");
+
+            using var hmac = new HMACSHA512();
+
+            var user = new User
+            {
+                Username = registerDto.Username.ToLower(),
+                Email = registerDto.Email.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            await _userRepository.AddAsync(user);
+            await _userRepository.SaveAllAsync();
+
+            return new UserDto
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Email = user.Email,
+                Token = CreateToken(user)
+            };
+        }
+
+        public async Task<UserDto> Login(LoginDto loginDto)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(loginDto.Username.ToLower());
+
+            if (user == null)
+                throw new Exception("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                    throw new Exception("Invalid password");
+            }
+
+            var allergies = await _userRepository.GetUserAllergiesAsync(user.Id);
+
+            return new UserDto
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Email = user.Email,
+                Allergies = allergies.Select(a => a.Code).ToList(),
+                Token = CreateToken(user)
+            };
+        }
+
+        public string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ YP_API/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using YP_API.DTOs;
+using YP_API.Models;
+
+namespace YP_API.Services
+{
+    public interface IAuthService
+    {
+        Task<UserDto> Register(RegisterDto registerDto);
+        Task<UserDto> Login(LoginDto loginDto);
+        string CreateToken(User user);
+    }
+}
