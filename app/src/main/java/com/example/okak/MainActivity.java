b106baa6d6bc096e4b08fde1ac0e139fd74ed20a
danik31-h <filippov31012007@gmail.com>
2025-11-01 11:03:00 +0500

    реализованный интерфейс

--- app/src/main/java/com/example/okak/MainActivity.java
+++ app/src/main/java/com/example/okak/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.okak;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+// ИСПРАВЛЕНИЕ: Импортируем NavHostFragment
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-///////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        // --- ИСПРАВЛЕНИЕ НАЧАЛО ---
+        // Это гарантированный способ получить NavController в onCreate
+        // Он находит сам NavHostFragment в менеджере фрагментов
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_activity_main);
+
+        // ... и затем получает контроллер у этого экземпляра
+        NavController navController = navHostFragment.getNavController();
+        // --- ИСПРАВЛЕНИЕ КОНЕЦ ---
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_search, R.id.navigation_shopping, R.id.navigation_favorites)
+                .build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
